88836ddd1cc4e344afcf0a791a717936d3bf3b19
Damian Bielecki <bielik20@gmail.com>
2019-03-29 09:13:15 +0100

    Add Commander

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,26 @@
 import chalk from 'chalk';
 import * as clear from 'clear';
+import * as commander from 'commander';
 import { textSync } from 'figlet';
-import * as minimist from 'minimist';
+
+const { version, description } = require('../package.json');
 
 export async function run() {
   clear();
   console.log(chalk.yellow(textSync('AdEng Jenkins', { horizontalLayout: 'full' })), '\n');
 
-  const args = minimist(process.argv.slice(2));
+  commander.version(version).description(description);
+  commander
+    .command('run')
+    .alias('r')
+    .description('Run Jenkins jobs')
+    .action(() => {
+      console.log('run');
+    });
 
-  let cmd = args._[0] || 'run';
-
-  if (args.version || args.v) {
-    cmd = 'version';
+  if (process.argv.length === 2) {
+    process.argv.push('-h');
   }
 
-  if (args.help || args.h) {
-    cmd = 'help';
-  }
-
-  switch (cmd) {
-    case 'version':
-      require('./commands/version').verion(args);
-      break;
-
-    case 'help':
-      require('./commands/help').help(args);
-      break;
-
-    default:
-      const { error } = require('./utils/error');
-      error(`"${cmd}" is not a valid command!`, true);
-  }
+  commander.parse(process.argv);
 }
--- src/utils/error.ts
+++
@@ -1,4 +0,0 @@
-export function error(message, exit) {
-  console.error(message);
-  exit && process.exit(1);
-}
