04e3f398dbc509658a8f46e2f4d02fbca0f66db4
Damian Bielecki <bielik20@gmail.com>
2020-02-03 16:08:06 +0100

    feat: add sandbox-adeng08

--- src/utils/sandbox.ts
+++ src/utils/sandbox.ts
@@ -6,6 +6,7 @@ export const sandboxes: Sandbox[] = [
   'sandbox-adeng05',
   'sandbox-adeng06',
   'sandbox-adeng07',
+  'sandbox-adeng08',
 ];
 
 export type Sandbox =
@@ -15,7 +16,8 @@ export type Sandbox =
   | 'sandbox-adeng04'
   | 'sandbox-adeng05'
   | 'sandbox-adeng06'
-  | 'sandbox-adeng07';
+  | 'sandbox-adeng07'
+  | 'sandbox-adeng08';
 
 export function isSandbox(input: string): input is Sandbox {
   return sandboxes.includes(input as Sandbox);
